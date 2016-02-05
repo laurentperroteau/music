5320bb89b2e6eb64daa28786646f068fd25e4204
laurentperroteau <laurentperroteau@gmail.com>
2016-02-05 18:39:51 +0100

    Move style in style.css

--- wp-content/themes/MusicAngular/index.php
+++ wp-content/themes/MusicAngular/index.php
@@ -1,38 +1,5 @@
 <?php get_header(); ?>
 
-    <style>
-        .wp-playlist { display: none }
-        audio {
-            background-color: red;
-            width: 100%;
-            box-shadow: 5px 5px 5px yellow;
-            border: 15px solid black;
-            font-size: 20px;
-        }
-        .wrapper {
-            width: 100%;
-            height: 100%;
-        }
-        .bgi {
-            position: fixed;
-            left: 0;
-            top: 0;
-            z-index: -1;
-            width: 100%;
-            height: 100%;
-            -webkit-filter: brightness(1);
-            -moz-filter: brightness(1);
-            filter: brightness(1);
-            -webkit-transition: -webkit-filter 500ms linear;
-            -moz-transition: -moz-filter 500ms linear;
-        }
-        .bgi.ctrlApplyFilter {
-            -webkit-filter: brightness(10);
-            -moz-filter: brightness(10);
-            filter: brightness(10);
-        }
-    </style>
-
     <div ng-controller="MusicApi as music" class="wrapper">
 
         <img
